e7a26e5e91d981e7140647d589a81c3984eae622
Prussiancatboy <134300632+Prussiancatboy@users.noreply.github.com>
2023-05-26 12:08:31 -0400

    Current progress save
    I have to take a break, so I am saving my current progress

--- promotions.py
+++ promotions.py
@@ -1,1 +1,22 @@
-class Promotions:
+import store
+import products
+from abc import ABC, abstractmethod
+
+class Promotions(ABC):
+    pass
+
+
+class SecondHalfPrice(Promotions):
+    pass
+
+class ThirdOneFree(Promotions):
+    pass
+
+class
+
+
+
+
+
+    third_one_free = promotions.ThirdOneFree("Third One Free!")
+    thirty_percent = promotions.PercentDiscount("30% off!", percent=30)
